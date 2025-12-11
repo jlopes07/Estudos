ab34ffdc9cd646eda3522944e6ef8e610e348f9e
Jessica <jessica.lopes93@hotmail.com>
2025-12-11 09:30:47 -0300

    refatorando DAL com EF

--- Alura/ScreenSound/ScreenSound-BD/ScreenSound/Banco/ScreenSoundContext.cs
+++ Alura/ScreenSound/ScreenSound-BD/ScreenSound/Banco/ScreenSoundContext.cs
@@ -7,22 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+internal class ScreenSoundContext: DbContext
 {
-    internal class ScreenSoundContext : DbContext
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Artista> Artistas { get; set; }
-
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-
-        public SqlConnection ObterConexao()
-        {
-            return new SqlConnection(connectionString);
-        }
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
